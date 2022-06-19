4a3c0ae85e04fd4a1c07244223fda94cb7fe9f07
G3N <fengyuan.yap@gmail.com>
2022-06-19 09:28:25 +0900

    Initial Commit

---
+++ Guide/GuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuideApp.swift
+//  Guide
+//
+//  Created by Feng Yuan Yap on 2022/06/19.
+//
+
+import SwiftUI
+
+@main
+struct GuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
